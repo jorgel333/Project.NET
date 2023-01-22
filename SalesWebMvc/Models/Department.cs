cf7b8dbc5fe111b65d52d3b21c093917c7e94854
Jorge Leonardo <jorgeleonardo333@gmail.com>
2023-01-21 23:17:20 -0300

    Implementing simple search and grouping search

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -28,7 +28,7 @@ namespace SalesWebMvc.Models
 
         public double TotalSales(DateTime initial, DateTime final)
         {
-            return Sellers.Sum(seller => TotalSales(initial, final));
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
     }
 }
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace SalesWebMvc
 {
@@ -22,3 +22,4 @@ namespace SalesWebMvc
                 .UseStartup<Startup>();
     }
 }
+
